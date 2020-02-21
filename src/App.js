d6068b3ee34e08e04143d5db39f5b763a5b4277f
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2020-02-21 18:25:31 +0530

    Fix pokemon img logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data'
+const { results } = data;
+
+function Pokemon(props) {
+  const { pokemonName, pokemonIndex } = props;
+  return <li>
+    <img style={{ "height": 60, "width": 60 }} src={"/sprites/" + pokemonIndex + ".png"} /><span>{pokemonName}</span>
+  </li>
+}
+
+
+const pokemonNames = results.map(obj => obj.name);
+const pokemonNameIndexMap = {}
+
+pokemonNames.forEach((pokemonName, index) => {
+  pokemonNameIndexMap[pokemonName] = index + 1
+})
+
+console.log({ pokemonNameIndexMap })
+
+function Pokedex() {
+
+
+  const [value, setValue] = useState("");
+  const filteredPokemonNames = pokemonNames.filter(pokemonName => pokemonName.includes(value))
+  const elements = filteredPokemonNames.map((pokemonName, index) => <Pokemon key={pokemonName} pokemonIndex={pokemonNameIndexMap[pokemonName]} pokemonName={pokemonName} />)
+  return <div>
+    <h1> PokeDex</h1>
+    <input value={value} onChange={(event) => {
+      setValue(event.target.value)
+    }} />
+    <ol>
+      {elements}
+    </ol>
+  </div>
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Learn Pokedex
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Pokedex />
       </header>
     </div>
   );
